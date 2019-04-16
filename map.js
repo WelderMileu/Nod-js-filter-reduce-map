c1dcc5ed45b6f7ac33c42e2c4ee46233746f4848
Welder Mileu <welderfelix21@gmail.com>
2019-04-15 21:06:02 -0300

    Estudando javascript funcional

---
+++ map.js
@@ -0,0 +1,34 @@
+let open = [3,2,4,5,3,2,7,8];
+
+const execute = open.map(( elem ) => {
+	return Math.round(elem);
+})
+
+console.log(execute)
+
+let time = [
+	{
+		nome:"Marilda",
+		idade:23,
+		"casado(a)":false
+	},
+	{
+		nome:"Paulo",
+		idade:45,
+		"casado(a)":true
+	},
+	{
+		nome:"Marina",
+		idade:23,
+		"casado(a)":true
+	}
+]
+
+const exc = time.map((elem) => {
+	return elem;
+})
+
+console.log(exc)
+
+
+
